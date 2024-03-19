967ca722821675e3a94754b48e64210808747645
Bullets-jyr <jyr@bullets.co.kr>
2024-03-19 22:28:17 +0900

    6. tutorial에서 사용하는 state 만드는 방식, state & cubit

---
+++ lib/cubits/counter/counter_state.dart
@@ -0,0 +1,29 @@
+part of 'counter_cubit.dart';
+
+class CounterState extends Equatable {
+  final int counter;
+
+  CounterState({
+    required this.counter,
+  });
+
+  factory CounterState.initial() {
+    return CounterState(counter: 0);
+  }
+
+  @override
+  List<Object> get props => [counter];
+
+  @override
+  String toString() {
+    return 'CounterState{counter: $counter}';
+  }
+
+  CounterState copyWith({
+    int? counter,
+  }) {
+    return CounterState(
+      counter: counter ?? this.counter,
+    );
+  }
+}
